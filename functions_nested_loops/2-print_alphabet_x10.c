420eac3d690d45cb237710d9ca03814b3fb8c698
Terencetjt26 <10597@holbertonstudents.com>
2025-02-18 11:49:27 +0100

    Your code outputs the alphabet correctly 10 times when you run the program, which is great

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,3 @@
-To summarize:
-
-Function prototype: void print_alphabet_x10(void);
+void print_alphabet_x10(void);
 Output: Prints the alphabet from a to z 10 times, each on a new line.
 Constraints: You used _putchar only twice in your code, following the given restrictions.
