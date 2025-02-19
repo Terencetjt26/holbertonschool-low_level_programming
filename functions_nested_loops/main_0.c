488a391c5e380cee7d9e3f336162be4a77adab60
Terencetjt26 <10597@holbertonstudents.com>
2025-02-19 12:02:09 +0100

    Task 8

--- functions_nested_loops/main_0.c
+++ functions_nested_loops/main_0.c
@@ -1,1 +1,15 @@
 #include "main.h"
+
+/* Déclaration de la fonction jack_bauer */
+void jack_bauer(void);
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    jack_bauer();
+    return (0);
+}
