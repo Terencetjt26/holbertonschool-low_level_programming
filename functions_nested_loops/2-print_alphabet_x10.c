948b5845b910a7b960c25aac39ccb586bf4637c8
Terencetjt26 <10597@holbertonstudents.com>
2025-02-19 10:31:03 +0100

    Task 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,14 @@
 #include "main.h"
 
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times in lowercase
+ */
 void print_alphabet_x10(void)
 {
-    char alphabet[27] = "abcdefghijklmnopqrstuvwxyz\n";
+    char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
     int i;
 
     for (i = 0; i < 10; i++)
-    {
-        char *ptr = alphabet;
-        while (*ptr)
-        {
-            _putchar(*ptr++);
-        }
-    }
+        _putchar(alphabet);
 }
+
