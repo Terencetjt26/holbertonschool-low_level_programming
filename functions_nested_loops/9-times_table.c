12c9927d71b1de98788bfa63581ef932fd3888c4
Terencetjt26 <10597@holbertonstudents.com>
2025-02-20 11:07:47 +0100

    Task 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ */
+void times_table(void)
+{
+    int i, j, product;
+
+    for (i = 0; i < 10; i++)  // Loop for each row (0 to 9)
+    {
+        for (j = 0; j < 10; j++)  // Loop for each column (0 to 9)
+        {
+            product = i * j;
+
+            if (j == 0)  // Print the first number of the row without a leading space
+                _putchar('0' + product);
+            else  // For subsequent numbers, handle formatting
+            {
+                _putchar(',');
+                _putchar(' ');
+
+                if (product < 10)  // If the product is a single digit
+                    _putchar(' ');
+                
+                _putchar('0' + product);  // Print the product
+            }
+        }
+        _putchar('\n');  // New line after each row
+    }
+}
