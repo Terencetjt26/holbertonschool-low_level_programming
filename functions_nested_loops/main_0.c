b153d45b59446dce2c972cfa39746b4b91206a4f
Terencetjt26 <10597@holbertonstudents.com>
2025-02-20 11:40:39 +0100

    Task 10

--- functions_nested_loops/main_0.c
+++ functions_nested_loops/main_0.c
@@ -1,4 +1,16 @@
 #include "8-24_hours.h"
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+
+    n = add(89, 9);  /* Appel de la fonction add */
+    printf("%d\n", n);
+    return (0);
+}
+
 
 void times_table(void);
 /* Déclaration de la fonction jack_bauer */
