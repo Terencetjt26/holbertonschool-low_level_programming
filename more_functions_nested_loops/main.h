59715df90c931e4e6d82eeeda47d8532e9579e55
Terencetjt26 <10597@holbertonstudents.com>
2025-02-24 15:52:51 +0100

    this function that draws a straight line in the terminal.

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,13 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
 
-int main(void);
-int _putchar(char c);
+void print_line(int n);
 void more_numbers(void);
 int _putchar(char c);
 void print_most_numbers(void);
-int _putchar(char c);
 void print_numbers(void);
 int mul(int a, int b);
 int _isdigit(int c);
