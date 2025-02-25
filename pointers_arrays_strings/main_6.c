3bd979704cbe3cfafa6909ec2fc6dbe11f2acdcd
Terencetjt26 <10597@holbertonstudents.com>
2025-02-25 15:49:49 +0100

    this function that convert a string to an integer.

--- pointers_arrays_strings/main_6.c
+++ pointers_arrays_strings/main_6.c
@@ -1,1 +1,27 @@
-welcome
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Test de la fonction _atoi
+ *
+ * Return: Toujours 0.
+ */
+int main(void)
+{
+    int nb;
+
+    nb = _atoi("98");
+    printf("%d\n", nb);
+    nb = _atoi("-402");
+    printf("%d\n", nb);
+    nb = _atoi("   ---++--98");
+    printf("%d\n", nb);
+    nb = _atoi("2147483647");
+    printf("%d\n", nb);
+    nb = _atoi("-2147483648");
+    printf("%d\n", nb);
+    nb = _atoi("Hello 1234 World");
+    printf("%d\n", nb);
+    
+    return (0);
+}
