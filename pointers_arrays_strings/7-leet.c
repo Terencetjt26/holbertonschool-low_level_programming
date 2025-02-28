e1b86a7233800e5b9bc1dcb8f91946feb28cb87a
Terencetjt26 <10597@holbertonstudents.com>
2025-02-28 11:05:35 +0100

    this function that encodes a string into 1337.

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - Encode une chaîne en "leet speak"
+ * @str: La chaîne à encoder
+ *
+ * Return: Pointeur vers la chaîne encodée
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
+
+	for (i = 0; str[i]; i++)
+	{
+		for (j = 0; letters[j]; j++)
+		{
+			if (str[i] == letters[j])
+			{
+				str[i] = numbers[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
