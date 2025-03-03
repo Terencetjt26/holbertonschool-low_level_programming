40267c3332ab1ef9f14976732788ced2abc4daca
Terencetjt26 <10597@holbertonstudents.com>
2025-03-03 16:38:18 +0100

    this function that copies memory area.

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,32 +1,28 @@
 #include "main.h"
 
 /**
-* _strncat - Concatène deux chaînes de caractères
-* en utilisant au maximum n octets de src
-* @dest: La destination de la concaténatio
-* @src: La source à ajouter
-* @n: Nombre maximum de caractères à ajouter
-*
-* Return: Pointeur vers la chaîne résultante dest
+* _strncat - Concatenates two strings up to n bytes
+* @dest: Destination string
+* @src: Source string
+* @n: Maximum number of bytes to copy
+* Return: Pointer to `dest`
 */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0, j = 0;
+	int i, j;
 
-	/* Trouver la fin de la chaîne dest */
-	while (dest[i] != '\0')
+	i = 0;
+	while (dest[i] != '\0') /* Trouver la fin de dest */
 		i++;
 
-	/* Ajouter au maximum n caractères de src à dest */
-	while (j < n && src[j] != '\0')
+	j = 0;
+	while (src[j] != '\0' && j < n) /* Ajouter src à dest jusqu'à n caractère */
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
 
-	/* Ajouter le caractère null à la fin */
-	dest[i] = '\0';
-
+	dest[i] = '\0'; /* Terminer la chaî */
 	return (dest);
 }
