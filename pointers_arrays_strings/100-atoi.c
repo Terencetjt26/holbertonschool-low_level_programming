aec0d9c25d4a8cea357ba7d532dc6a93bb3ddce2
Terencetjt26 <10597@holbertonstudents.com>
2025-03-05 17:02:45 +0100

    this function that convert a string to an integer.

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,34 +1,38 @@
 #include "main.h"
 
 /**
- * _atoi - Convertit une chaîne en entier.
- * @s: La chaîne à convertir.
- *
- * Return: L'entier converti, ou 0 si aucun chiffre trouvé.
- */
+* _atoi - Convertit une chaîne de caractères en entier
+* @s: La chaîne à convertir
+*
+* Return: L'entier converti
+*/
 int _atoi(char *s)
 {
-	int i = 0;
-	int sign = 1;
-	int result = 0;
-	int found_digit = 0;
+	int i = 0, sign = 1;
+	unsigned int result = 0;
 
-	/* Parcourir la chaîne */
-	while (s[i])
+	/* Gérer les espaces et caractères non numériques */
+	while (s[i] && (s[i] < '0' || s[i] > '9'))
 	{
-		/* Gérer les signes */
 		if (s[i] == '-')
 			sign *= -1;
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			result = result * 10 + (s[i] - '0');
-			found_digit = 1;
-		}
-		else if (found_digit)
-			break; /* Stopper après le premier nombre détecté */
-
 		i++;
 	}
 
-	return (result * sign);
+	/* Convertir les chiffres en entier */
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		if (result > 214748364 || (result == 214748364 && s[i] > '7'))
+		{
+			/* Vérifier si INT_MIN est géré correctement */
+			if (sign == -1 && s[i] == '8' && result == 214748364)
+				return (-2147483648);
+			return (sign == 1 ? 2147483647 : -2147483648);
+		}
+
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+
+	return (sign * result);
 }
