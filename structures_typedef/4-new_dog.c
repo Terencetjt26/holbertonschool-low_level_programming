b648292f9587202ca36abcad399685ba8b270604
Terencetjt26 <10597@holbertonstudents.com>
2025-03-13 16:33:51 +0100

    this function that creates a new dog.

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+* new_dog - Crée un nouveau chien
+* @name: Nom du chien
+* @age: Âge du chien
+* @owner: Propriétaire du chien
+* 
+* Return: Pointeur vers le nouveau chien, ou NULL si échec
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+	char *new_name, *new_owner;
+	int i, name_len = 0, owner_len = 0;
+
+	/* Allocation de mémoire pour le chie */
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
+
+	/* Calcul de la longueur des chaînes */
+	while (name[name_len])
+		name_len++;
+	while (owner[owner_len])
+		owner_len++;
+
+	/* Allocation de mémoire pour les chaînes */
+	new_name = malloc(sizeof(char) * (name_len + 1));
+	new_owner = malloc(sizeof(char) * (owner_len + 1));
+
+	if (new_name == NULL || new_owner == NULL)
+	{
+		free(new_name);
+		free(new_owner);
+		free(d);
+		return (NULL);
+	}
+
+	/* Copie des chaînes */
+	for (i = 0; i <= name_len; i++)
+		new_name[i] = name[i];
+	for (i = 0; i <= owner_len; i++)
+		new_owner[i] = owner[i];
+
+	/* Initialisation de la structure */
+	d->name = new_name;
+	d->age = age;
+	d->owner = new_owner;
+
+	return (d);
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,8 +7,7 @@
 * @age: Âge du chien (nombre flottant)
 * @owner: Nom du propriétaire (chaîne de caractères)
 */
-struct dog
-{
+struct dog {
 	char *name;
 	float age;
 	char *owner;
@@ -18,5 +17,8 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
