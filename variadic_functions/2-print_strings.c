0e97bf6d5a439c3d4cf336900ceded7549bff080
terencetjt26 <Terencetjt26>
2025-03-24 14:53:05 +0000

    this function that prints strings, followed by a new line.

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -10,19 +10,19 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
-	char *str;
+	const char *str;
 
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(args, char *);
+		str = va_arg(args, const char *);
 		if (str == NULL)
 			printf("(nil)");
 		else
 			printf("%s", str);
 
-		if (separator != NULL && i < n - 1)
+		if (i < n - 1 && separator != NULL)
 			printf("%s", separator);
 	}
 
