6eb8c4c6b6f4b57f7d21a4f094f309586a128c85
Terencetjt26 <10597@holbertonstudents.com>
2025-04-04 09:00:02 +0000

    add print_binary with custom _putchar for binary output

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -7,7 +7,7 @@
 void print_binary(unsigned long int n)
 {
 	if (n > 1)
-		print_binary(n >> 1); /* Décalage à droite pour traiter les bits plus significatifs */
+		print_binary(n >> 1);
 
 	_putchar((n & 1) + '0'); /* Affiche le bit courant (0 ou 1) */
 }
