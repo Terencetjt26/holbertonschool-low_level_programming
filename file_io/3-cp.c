30d05ab8529e844247cfa8922b0e9291fdb0dc64
Terencetjt26 <10597@holbertonstudents.com>
2025-04-07 13:35:33 +0000

    Refactor cp to split main into smaller functions for readability and betty compliance

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,75 +1,59 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <errno.h>
 
-#define BUFFER_SIZE 1024
+#define BUF_SIZE 1024
 
 /**
-* print_error - prints an error message and exits with the given code
-* @exit_code: the exit code
-* @format: error message format
-* @arg: argument to print in the error message
+* error_exit - Print error and exit with given code
 */
-void print_error(int exit_code, const char *format, const char *arg)
+void error_exit(int code, const char *msg, const char *filename, int fd)
 {
-	dprintf(STDERR_FILENO, format, arg);
-	exit(exit_code);
+	if (filename)
+		dprintf(STDERR_FILENO, msg, filename);
+	else
+		dprintf(STDERR_FILENO, msg, fd);
+	exit(code);
 }
 
 /**
-* main - copies content of a file to another file
-* @ac: number of arguments
-* @av: array of arguments
-*
-* Return: 0 on success, exits otherwise
+* main - copies the content of a file to another file
+* @ac: argument count
+* @av: argument vector
+* Return: 0 on success, exits with error codes on failure
 */
 int main(int ac, char **av)
 {
 	int fd_from, fd_to, r, w;
-	char buffer[BUFFER_SIZE];
+	char buffer[BUF_SIZE];
 
 	if (ac != 3)
-		print_error(97, "Usage: cp file_from file_to\n", "");
+		error_exit(97, "Usage: cp file_from file_to\n", NULL, 0);
 
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
-		print_error(98, "Error: Can't read from file %s\n", av[1]);
+		error_exit(98, "Error: Can't read from file %s\n", av[1], 0);
 
 	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
-	{
-		close(fd_from);
-		print_error(99, "Error: Can't write to %s\n", av[2]);
-	}
+		error_exit(99, "Error: Can't write to %s\n", av[2], 0);
 
-	while ((r = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	while ((r = read(fd_from, buffer, BUF_SIZE)) > 0)
 	{
 		w = write(fd_to, buffer, r);
 		if (w != r)
-		{
-			close(fd_from);
-			close(fd_to);
-			print_error(99, "Error: Can't write to %s\n", av[2]);
-		}
+			error_exit(99, "Error: Can't write to %s\n", av[2], 0);
 	}
 
 	if (r == -1)
-		print_error(98, "Error: Can't read from file %s\n", av[1]);
+		error_exit(98, "Error: Can't read from file %s\n", av[1], 0);
 
 	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
+		error_exit(100, "Error: Can't close fd %d\n", NULL, fd_from);
 
 	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
+		error_exit(100, "Error: Can't close fd %d\n", NULL, fd_to);
 
 	return (0);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -13,4 +13,13 @@ int create_file(const char *filename, char *text_content);
 
 int append_text_to_file(const char *filename, char *text_content);
 
+int _putchar(char c);
+
+void close_file(int fd);
+
+void print_error(int exit_code, const char *format, const char *arg);
+
+void copy_file(const char *file_from, const char *file_to);
+
+
 #endif /* MAIN_H */
